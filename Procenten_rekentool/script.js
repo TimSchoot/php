c72d4bc8e58c9c2529f32727ee29a891a3ea1288
TimSchoot <t.vanderschoot@student.alfa-college.nl>
2024-04-08 11:26:40 +0200

    solveProblem

--- Procenten_rekentool/script.js
+++ Procenten_rekentool/script.js
@@ -1,34 +1,95 @@
-const oud = document.getElementById("oud")
+const oud = document.getElementById("oud");
 
-const soort = document.getElementById("soort")
-const percentage = document.getElementById("percentage")
+const soort = document.getElementById("soort");
+const perc = document.getElementById("percentage");
 
-const nieuw = document.getElementById("nieuw")
+const nieuw = document.getElementById("nieuw");
 
-function checkInput() {
-    if (oud.value != '') {
-        oud.classList.remove("is-invalid");
-        oud.classList.add("is-valid");
+const losopknop = document.getElementById("losop_btn");
+
+const factor = document.getElementById("vFactor");
+const deler = document.getElementById("dFactor");
+
+let ber_factor
+
+function checkInput()
+{
+    if(oud.value != "" && soort.value != "" && perc.value != "")
+    {
+        losopknop.disabled = false
+        oud.classList.remove("is-invalid")
+        oud.classList.add("is-valid")
+        soort.classList.remove("is-invalid")
+        soort.classList.add("is-valid")
+        perc.classList.remove("is-invalid")
+        perc.classList.add("is-valid")
     }
-    if (nieuw.value != '') {
-        nieuw.classList.remove("is-invalid");
-        nieuw.classList.add("is-valid");
+    else if (nieuw.value != "" && soort.value != "" && perc.value != "")
+    {
+        losopknop.disabled = false
+        nieuw.classList.remove("is-invalid")
+        nieuw.classList.add("is-valid")
+        soort.classList.remove("is-invalid")
+        soort.classList.add("is-valid")
+        perc.classList.remove("is-invalid")
+        perc.classList.add("is-valid")
     }
-    if (soort.value != '') {
-        soort.classList.remove("is-invalid");
-        soort.classList.add("is-valid");
-    }
-    if (percentage.value != '') {
-        percentage.classList.remove("is-invalid");
-        percentage.classList.add("is-valid");
-    }
-    if (oud.value != '' && nieuw.value != '') {
-        document.getElementById("losop_btn").disabled = false;
+    else if (oud.value != "" && nieuw.value != "")
+    {
+        losopknop.disabled = false
+        oud.classList.remove("is-invalid")
+        oud.classList.add("is-valid")
+        nieuw.classList.remove("is-invalid")
+        nieuw.classList.add("is-valid")
     }
 }
 
-
 function solveProblem()
 {
-    alert("hij doet het")
+    if(oud.value != "" && soort.value != "" && perc.value != "")
+    {
+        if(soort.value == 0)
+            ber_factor = perc.value / 100
+        else if(soort.value == 1)
+            ber_factor = 1 + perc.value / 100
+        else
+            ber_factor = 1 - perc.value / 100
+        factor.value = ber_factor
+        deler.value = ber_factor
+        nieuw.value = oud.value * ber_factor
+    }
+    else if (nieuw.value != "" && soort.value != "" && perc.value != "")
+    {
+        if(soort.value == 0)
+            ber_factor = perc.value / 100
+        else if(soort.value == 1)
+            ber_factor = 1 + perc.value / 100
+        else
+            ber_factor = 1 - perc.value / 100
+        factor.value = ber_factor
+        deler.value = ber_factor
+        oud.value = nieuw.value / ber_factor
+    }
+    else
+    {
+        ber_factor = (nieuw.value / oud.value).toFixed(4)
+        factor.value = ber_factor
+        deler.value = ber_factor
+        if(ber_factor > 1)
+        {
+            soort.value = 1
+            perc.value = ((ber_factor - 1) * 100).toFixed(2)
+        }
+        else
+        {
+            soort.value = 2
+            perc.value = ((1 - ber_factor) * 100).toFixed(2)
+        }
+    }
+    oud.disabled = true
+    nieuw.disabled = true
+    soort.disabled = true
+    perc.disabled = true
+    losopknop.disabled = true
+    alert ("je kunt opnieuw beginnen door een re-load")
 }
